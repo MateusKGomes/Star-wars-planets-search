178fce4022aaed3a8c921f30c965eee3cd823ece
Mateus Kulik Gomes <mateuskulik30@gmail.com>
2023-02-27 18:41:33 -0300

    Requisito 2, adiciona filtro por nome

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,80 +1,93 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import Mycontext from '../context/myContext';
 
 export default function Table() {
+  const [search, setSearch] = useState('');
   const {
     data,
   } = useContext(Mycontext);
 
-  console.log(data);
-
+  const filteredByName = data.filter(
+    (item) => item.name.includes(search),
+  );
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Rotation Period</th>
-          <th>Orbital Period</th>
-          <th>Diameter</th>
-          <th>Climate</th>
-          <th>Gravity</th>
-          <th>Terrain</th>
-          <th>Surface Water</th>
-          <th>Population</th>
-          <th>films</th>
-          <th>Created</th>
-          <th>Edited</th>
-          <th>URl</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          data.map((planet) => (
-            <tr key={ planet.name }>
-              <th>
-                {planet.name}
-              </th>
-              <th>
-                { planet.rotation_period}
-              </th>
-              <th>
-                { planet.orbital_period}
-              </th>
-              <th>
-                { planet.diameter}
-              </th>
-              <th>
-                { planet.climate}
-              </th>
-              <th>
-                { planet.gravity}
-              </th>
-              <th>
-                { planet.terrain}
-              </th>
-              <th>
-                { planet.surface_water}
-              </th>
-              <th>
-                { planet.population}
-              </th>
-              <th>
-                { planet.films}
-              </th>
-              <th>
-                { planet.created}
-              </th>
-              <th>
-                { planet.edited}
-              </th>
-              <th>
-                { planet.url}
-              </th>
+    <div>
+      <input
+        type="text"
+        value={ search }
+        data-testid="name-filter"
+        onChange={ ({ target }) => {
+          setSearch(target.value);
+        } }
+      />
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>films</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URl</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            filteredByName.map((planet) => (
+              <tr key={ planet.name }>
+                <th>
+                  {planet.name}
+                </th>
+                <th>
+                  { planet.rotation_period}
+                </th>
+                <th>
+                  { planet.orbital_period}
+                </th>
+                <th>
+                  { planet.diameter}
+                </th>
+                <th>
+                  { planet.climate}
+                </th>
+                <th>
+                  { planet.gravity}
+                </th>
+                <th>
+                  { planet.terrain}
+                </th>
+                <th>
+                  { planet.surface_water}
+                </th>
+                <th>
+                  { planet.population}
+                </th>
+                <th>
+                  { planet.films}
+                </th>
+                <th>
+                  { planet.created}
+                </th>
+                <th>
+                  { planet.edited}
+                </th>
+                <th>
+                  { planet.url}
+                </th>
 
-            </tr>
-          ))
-        }
-      </tbody>
-    </table>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    </div>
+
   );
 }
